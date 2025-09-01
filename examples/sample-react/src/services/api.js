b055834b8ed517f1a9fc3d9466ac3a38c1d8fc36
Stalin Jose <stalin.jose1234@hotmail.com>
2025-09-01 18:08:33 +0530

    server url changed

--- examples/sample-react/src/services/api.js
+++ examples/sample-react/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://questify-server-gdegcmawbde8hxfn.centralindia-01.azurewebsites.net';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://ground-cite-server-brhcaaa4b2ezhwep.centralindia-01.azurewebsites.net';
 
 // Create axios instance with default config
 const api = axios.create({
